84fa05c12e9b682a69db33d10129db57de054cbc
Eddi3MS <edsonmarc2014@gmail.com>
2022-06-21 14:29:15 -0300

    to fixed added

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,19 +1,19 @@
 const inches = document.getElementById("inches") as HTMLInputElement;
 const centim = document.getElementById("centimeter") as HTMLInputElement;
 
-function conversor(inc: number, cm: number, type: string) {
+function conversor(inc: number, cm: number, type: string = "cen") {
   if (type === "inc") {
     centim.value = (inc * 2.54).toString();
     return;
   }
 
-  inches.value = (cm * 0.393701).toString();
+  inches.value = (cm * 0.393701).toFixed(3).toString();
 }
 
-inches.addEventListener("input", function (e) {
+inches.addEventListener("input", function () {
   conversor(+this.value, 0, "inc");
 });
 
-centim.addEventListener("input", function (e) {
-  conversor(0, +this.value, "cen");
+centim.addEventListener("input", function () {
+  conversor(0, +this.value);
 });
